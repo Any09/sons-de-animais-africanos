fdf5bc3e8318a0992551711b73de5bf8f0ae40c5
Anny <104777994+Any09@users.noreply.github.com>
2023-11-10 17:48:18 -0300

    eeye

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function tocaSomLeão(){
     document.querySelector('#som_tecla_leão').play();
 }
+function tocaSomTigre(){
+    document.querySelector('#som_tecla_tigre').play();
+}
+function tocaSomElefante(){
+    document.querySelector('#som_tecla_elefante').play();
+}
