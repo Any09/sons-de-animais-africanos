82838cfc47c86d9aaf40022f54023d652baaa488
Anny <104777994+Any09@users.noreply.github.com>
2023-11-17 16:23:57 -0300

    ufuy

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 function tocaSomLeão(){
     document.querySelector('#som_tecla_leão').play();
 }
+document.querySelector('.tecla_leão').onclick = tocaSomLeão;
+
 function tocaSomTigre(){
     document.querySelector('#som_tecla_tigre').play();
 }
+document.querySelector('.tecla_tigre').onclick = tocaSomTigre;
+
 function tocaSomElefante(){
     document.querySelector('#som_tecla_elefante').play();
 }
+document.querySelector('.tecla_elefante').onclick = tocaSomElefante;
+
 function tocaSomRinoceronte(){
     document.querySelector('#som_tecla_leão').play();
 }
+document.querySelector('.tecla_rinoceronte').onclick = tocaSomRinoceronte;
+
 function tocaSomBúfalo(){
     document.querySelector('#som_tecla_tigre').play();
 }
+document.querySelector('.tecla_búfalo').onclick = tocaSomBúfalo
+
 function tocaSomCrocodilo(){
     document.querySelector('#som_tecla_elefante').play();
 }
+document.querySelector('.tecla_crocodilo').onclick = tocaSomCrocodilo;
+
 function tocaSomLeopardo(){
     document.querySelector('#som_tecla_leopardo').play();
 }
+document.querySelector('.tecla_leopardo').onclick = tocaSomLeopardo;
+
 function tocaSomPuma(){
     document.querySelector('#som_tecla_puma').play();
 }
+document.querySelector('.tecla_puma').onclick = tocaSomPuma;
+
 function tocaSomGorila(){
     document.querySelector('#som_tecla_gorila').play();
 }
+document.querySelector('.tecla_gorila').onclick = tocaSomGorila;
