8d901c5f21bb9dabf799c79b70ecbff84052c770
Anny <104777994+Any09@users.noreply.github.com>
2023-11-17 16:33:24 -0300

    fyukj

--- main.js
+++ main.js
@@ -3,40 +3,48 @@ function tocaSomLeão() {
 }
 document.querySelector('.tecla_leão').onclick = tocaSomLeão;
 
+
 function tocaSomTigre() {
     document.querySelector('#som_tecla_tigre').play();
 }
 document.querySelector('.tecla_tigre').onclick = tocaSomTigre;
+
 
 function tocaSomElefante() {
     document.querySelector('#som_tecla_elefante').play();
 }
 document.querySelector('.tecla_elefante').onclick = tocaSomElefante;
 
+
 function tocaSomRinoceronte() {
-    document.querySelector('#som_tecla_leão').play();
+    document.querySelector('#som_tecla_rinoceronte').play();
 }
 document.querySelector('.tecla_rinoceronte').onclick = tocaSomRinoceronte;
 
+
 function tocaSomBúfalo() {
-    document.querySelector('#som_tecla_tigre').play();
+    document.querySelector('#som_tecla_búfalo').play();
 }
-document.querySelector('.tecla_búfalo').onclick = tocaSomBúfalo
+document.querySelector('.tecla_búfalo').onclick = tocaSomBúfalo;
+
 
 function tocaSomCrocodilo() {
-    document.querySelector('#som_tecla_elefante').play();
+    document.querySelector('#som_tecla_crocodilo').play();
 }
 document.querySelector('.tecla_crocodilo').onclick = tocaSomCrocodilo;
+
 
 function tocaSomLeopardo() {
     document.querySelector('#som_tecla_leopardo').play();
 }
 document.querySelector('.tecla_leopardo').onclick = tocaSomLeopardo;
 
+
 function tocaSomPuma() {
     document.querySelector('#som_tecla_puma').play();
 }
 document.querySelector('.tecla_puma').onclick = tocaSomPuma;
+
 
 function tocaSomGorila() {
     document.querySelector('#som_tecla_gorila').play();
